5b874f75571285788abaf6a7f7c984f7d1a3ac92
Ngay Vong <ngayvong5190@gmail.com>
2020-10-05 12:50:42 -0400

    Add gif to show app functionalities

--- CollectionView_TableView/Screens/CollectionView/BookList/Controller/BookListCollectionViewController.swift
+++ CollectionView_TableView/Screens/CollectionView/BookList/Controller/BookListCollectionViewController.swift
@@ -18,13 +18,7 @@ class BookListCollectionViewController: UICollectionViewController {
     // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-        
-        // Register cell classes
-        //self.collectionView!.register(BookCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
-        
+
         // Do any additional setup after loading the view.
         getDataFromServer()
         setupUI()
@@ -63,7 +57,6 @@ class BookListCollectionViewController: UICollectionViewController {
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
         return itemInfos.count
     }
     
@@ -88,33 +81,4 @@ class BookListCollectionViewController: UICollectionViewController {
         
         self.show(detailVC, sender: self)
     }
-    
-    /*
-     // Uncomment this method to specify if the specified item should be highlighted during tracking
-     override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment this method to specify if the specified item should be selected
-     override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-     return true
-     }
-     */
-    
-    /*
-     // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-     override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-     return false
-     }
-     
-     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-     
-     }
-     */
 }
